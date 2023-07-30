1ab5deaf7088ce13846210b4e538c292856d3a82
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-07-30 18:45:27 +0800

    Added wezterm config
    Also removed the neovim config folder for now, since it doesn't link to
    the other repository.

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- Set the colour scheme to Bluloco Dark
+config.color_scheme = "Bluloco Dark"
+
+-- Set the font to Cascadia Code Nerd Font
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+
+-- Finally, return the configuration to wezterm
+return config
