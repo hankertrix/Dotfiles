feffc5ccab88c100e773b1c6e56779843e2cc93f
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-08-06 00:00:02 +0800

    Made the volume notification look better
    Added an additional space between the volume icon and the volume level
    so that it doesn't look like the icon is clipping the volume level.

--- .config/i3/scripts/volume_brightness.sh
+++ .config/i3/scripts/volume_brightness.sh
@@ -46,7 +46,7 @@ function get_brightness_icon {
 function show_volume_notif {
     volume=$(get_mute)
     get_volume_icon
-    dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal "$volume_icon $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
+    dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal "$volume_icon  $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
 }
 
 # Displays a brightness notification using dunstify
