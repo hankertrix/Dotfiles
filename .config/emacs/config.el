995b72717ea03d0a8f9d33c92388e69d943fb437
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-08-18 03:15:19 +0800

    Rearranged the cape completion functions
    Pushed the completion for TeX commands to the bottom and made the file
    completion lower in priority as I rarely use the completion for the file.

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -684,13 +684,13 @@
 
   ;; Initialise cape and add the wanted completion functions
   :init
-  (add-to-list 'completion-at-point-functions #'cape-tex)
   (add-to-list 'completion-at-point-functions #'cape-keyword)
   (add-to-list 'completion-at-point-functions #'cape-elisp-block)
-  (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-dict)
+  (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   (add-to-list 'completion-at-point-functions #'cape-history)
+  (add-to-list 'completion-at-point-functions #'cape-tex)
   ;; (add-to-list 'completion-at-point-functions #'cape-sgml)
   ;; (add-to-list 'completion-at-point-functions #'cape-rfc1345)
   ;; (add-to-list 'completion-at-point-functions #'cape-abbrev)
