80cfd545331e208aea2ca3d2a5aded9a4d027a57
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-12-29 14:29:33 +0800

    Added org-ascii-text-width as a safe local variable

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -9,7 +9,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil-collection use-package evil)))
+ '(package-selected-packages '(evil-collection use-package evil))
+ '(safe-local-variable-values '((org-ascii-text-width . 50))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
