4be8b7a7cd603a6b69db5742c16e859a210c8a79
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-12-29 14:34:00 +0800

    Actually fixed the vim-fugitive highlighting

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -6,9 +6,15 @@ local utils = require("utils")
 -- Set a key map to open Git
 vim.keymap.set("n", "<Leader>gs", vim.cmd.Git, { desc = "Opens Git" })
 
--- Set the highlight groups to the correct colours
-vim.api.nvim_set_hl(0, "diffAdded", { link = "DiffviewStatusAdded" })
-vim.api.nvim_set_hl(0, "diffRemoved", { link = "DiffviewStatusDeleted" })
+-- The function to configure vim-fugitive
+local function fugitive_config()
+
+    -- Set the highlight groups to the correct colours
+    vim.api.nvim_set_hl(0, "diffAdded", { link = "DiffviewStatusAdded" })
+    vim.api.nvim_set_hl(0, "diffRemoved", { link = "DiffviewStatusDeleted" })
+
+end
+
 
 -- Returns the fugitive module for lazy.nvim
 return {
@@ -16,6 +22,7 @@ return {
     cmd = "Git",
     lazy = true,
     cond = utils.firenvim_not_active,
+    config = fugitive_config
     -- event = "User InGitRepo"
 }
 
