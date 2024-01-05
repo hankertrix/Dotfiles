25fcd92816ee4bf61a61bf508ef34992e835d694
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-01-05 12:22:44 +0800

    Renamed gitsigns to be more generic
    There are also other plugins that do the same thing, so making the
    module more generic is going to help with migration.

--- .config/nvim/lua/plugins/git_gutter.lua
+++ .config/nvim/lua/plugins/git_gutter.lua
@@ -1,20 +1,21 @@
--- Gitsigns configuration
+-- Git gutter plugin configuration
+-- Currently it's gitsigns
 
 -- Gets the module with the utilities
 local utils = require("utils")
 
--- Function to set up gitsigns
+-- Function to set up the git gutter plugin
 local function setup()
 
     -- Stops executing if the packages aren't installed
     if not utils.status_ok("gitsigns") then return end
 
-    -- Set up gitsigns
+    -- Set up the git gutter plugin
     require("gitsigns").setup()
 
 end
 
--- Returns the gitsigns module for lazy.nvim
+-- Returns the git gutter module for lazy.nvim
 return {
     "lewis6991/gitsigns.nvim",
     config = setup,
