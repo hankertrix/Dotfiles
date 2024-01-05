0cd181983ff58f03e7862720cee6d34db82af684
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-01-05 12:51:12 +0800

    Sets up -> set up

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -9,7 +9,7 @@ local function setup()
     -- Stops executing if the package isn't installed
     if not utils.status_ok("aerial") then return end
 
-    -- Sets up aerial
+    -- Set up aerial
     require("aerial").setup({
         attach_mode = "global",
         icons = require("shared_configs").lsp_kind_icons
