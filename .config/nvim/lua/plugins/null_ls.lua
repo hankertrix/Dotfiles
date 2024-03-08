20c9e9095c75878f83d25680f89ca57872a8f9ed
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-03-08 13:09:18 +0800

    Add CSpell

--- .config/nvim/lua/plugins/null_ls.lua
+++ .config/nvim/lua/plugins/null_ls.lua
@@ -7,10 +7,41 @@ local utils = require("utils")
 local function setup()
 
     -- Stops executing if the packages aren't installed
-    if not utils.status_ok({ "mason-null-ls", "null-ls" }) then return end
+    if not utils.status_ok({
+        "mason-null-ls",
+        "null-ls",
+        "cspell"
+    }) then return end
 
     -- Gets the null-ls module
     local null_ls = require("null-ls")
+
+    -- Gets the CSpell module
+    local cspell = require("cspell")
+
+    -- The CSpell config
+    local cspell_config = {
+
+        -- Set the severity of CSpell to hint
+        diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity = vim.diagnostic.severity["HINT"]
+        end,
+
+        -- The actual configuration for CSpell
+        config = {
+
+            -- Format the JSON file when adding to the CSpell config file
+            on_add_to_json = function(payload)
+                os.execute(
+                    string.format(
+                        "cat %s | jq -S '.words |= sort' | tee %s > /dev/null",
+                        payload.cspell_config_path,
+                        payload.cspell_config_path
+                    )
+                )
+            end
+        },
+    }
 
     -- Set up null-ls
     null_ls.setup {
@@ -31,6 +62,11 @@ local function setup()
 
             -- Linters
 
+            -- Code spelling
+            cspell.diagnostics.with(cspell_config),
+            cspell.code_actions.with(cspell_config),
+
+
             -- Miscellaneous
             null_ls.builtins.code_actions.gitsigns
         }
@@ -42,10 +78,10 @@ local function setup()
         -- Ensure nothing is installed (use null-ls configuration)
         ensure_installed = nil,
 
-        -- Automatically install based on the null-ls configuration
-        automatic_installation = true,
+        -- Don't automatically install based on the null-ls configuration
+        -- Use mason tool installer instead
+        automatic_installation = false,
     }
-
 end
 
 -- Returns the mason-null-ls plugin to lazy.nvim
@@ -56,6 +92,9 @@ return {
     lazy = true,
     dependencies = {
         "mason.nvim",
-        { "nvimtools/none-ls.nvim", dependencies = "gitsigns.nvim" }
+        {
+            "nvimtools/none-ls.nvim",
+            dependencies = { "gitsigns.nvim", "davidmh/cspell.nvim" }
+        }
     }
 }
