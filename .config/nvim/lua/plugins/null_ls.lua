21ccd9a32fff28af1f8bbb523c6f2dfbc83aca42
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-03-08 13:29:48 +0800

    Remove mason-null-ls
    Use mason-tool-installer instead.

--- .config/nvim/lua/plugins/null_ls.lua
+++ .config/nvim/lua/plugins/null_ls.lua
@@ -7,11 +7,7 @@ local utils = require("utils")
 local function setup()
 
     -- Stops executing if the packages aren't installed
-    if not utils.status_ok({
-        "mason-null-ls",
-        "null-ls",
-        "cspell"
-    }) then return end
+    if not utils.status_ok({ "null-ls", "cspell" }) then return end
 
     -- Gets the null-ls module
     local null_ls = require("null-ls")
@@ -71,30 +67,17 @@ local function setup()
             null_ls.builtins.code_actions.gitsigns
         }
     }
-
-    -- Set up mason-null-ls
-    require("mason-null-ls").setup {
-
-        -- Ensure nothing is installed (use null-ls configuration)
-        ensure_installed = nil,
-
-        -- Don't automatically install based on the null-ls configuration
-        -- Use mason tool installer instead
-        automatic_installation = false,
-    }
 end
 
--- Returns the mason-null-ls plugin to lazy.nvim
+-- Returns the none-ls plugin to lazy.nvim
 return {
-    "jay-babu/mason-null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     config = setup,
     event = { "BufReadPost", "BufNewFile", "BufWritePre" },
     lazy = true,
     dependencies = {
         "mason.nvim",
-        {
-            "nvimtools/none-ls.nvim",
-            dependencies = { "gitsigns.nvim", "davidmh/cspell.nvim" }
-        }
+        "gitsigns.nvim",
+        "davidmh/cspell.nvim"
     }
 }
