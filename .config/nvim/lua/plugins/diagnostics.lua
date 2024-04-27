bd6143356c4d04905e8452a2755c6a7bb41f2b78
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-04-27 12:41:44 +0800

    Reformat the file

--- .config/nvim/lua/plugins/diagnostics.lua
+++ .config/nvim/lua/plugins/diagnostics.lua
@@ -11,9 +11,9 @@ local function setup()
     if not utils.status_ok("trouble") then return end
 
     -- Set up the diagnostics plugin
-    require("trouble").setup {
-        signs = require("shared_configs").lsp_kind_icons
-    }
+    require("trouble").setup({
+        signs = require("shared_configs").lsp_kind_icons,
+    })
 end
 
 -- Return the diagnostics plugin for lazy.nvim
@@ -24,11 +24,53 @@ return {
     cmd = { "TroubleToggle", "Trouble" },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-        { "<Leader>tt", function() require("trouble").toggle() end, mode = "n", desc = "Toggles the Trouble window" },
-        { "<Leader>tw", function() require("trouble").toggle("workspace_diagnostics") end, mode = "n", desc = "Display diagnostics for all open buffers in a Trouble window" },
-        { "<Leader>tb", function() require("trouble").toggle("document_diagnostics") end, mode = "n", desc = "Display buffer diagnostics in a Trouble window" },
-        { "<Leader>tq", function() require("trouble").toggle("quickfix") end, mode = "n", desc = "Display quick fix items in a Trouble window" },
-        { "<Leader>tl", function() require("trouble").toggle("loclist") end, mode = "n", desc = "Display the window's location list items in a Trouble window" },
-        { "gR", function() require("trouble").toggle("lsp_references") end, mode = "n", desc = "Display all references in a Trouble window" },
-    }
+        {
+            "<Leader>tt",
+            function()
+                require("trouble").toggle()
+            end,
+            mode = "n",
+            desc = "Toggles the Trouble window",
+        },
+        {
+            "<Leader>tw",
+            function()
+                require("trouble").toggle("workspace_diagnostics")
+            end,
+            mode = "n",
+            desc = "Display diagnostics for all open buffers in a Trouble window",
+        },
+        {
+            "<Leader>tb",
+            function()
+                require("trouble").toggle("document_diagnostics")
+            end,
+            mode = "n",
+            desc = "Display buffer diagnostics in a Trouble window",
+        },
+        {
+            "<Leader>tq",
+            function()
+                require("trouble").toggle("quickfix")
+            end,
+            mode = "n",
+            desc = "Display quick fix items in a Trouble window",
+        },
+        {
+            "<Leader>tl",
+            function()
+                require("trouble").toggle("loclist")
+            end,
+            mode = "n",
+            desc = "Display the window's location list items in a Trouble window",
+        },
+        {
+            "gR",
+            function()
+                require("trouble").toggle("lsp_references")
+            end,
+            mode = "n",
+            desc = "Display all references in a Trouble window",
+        },
+    },
 }
