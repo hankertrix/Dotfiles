3140a1eb7308c05e81a80c57b5e81595f8293ab1
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-04-27 12:52:53 +0800

    Use yazi.nvim instead of mini.files

--- .config/nvim/lua/plugins/file_explorer.lua
+++ .config/nvim/lua/plugins/file_explorer.lua
@@ -1,45 +1,31 @@
 -- The configuration for the file explorer
--- Current the plugin used is mini.files
+-- Currently, the plugin used is yazi.nvim
 
 -- Gets the module with the utilities
 local utils = require("utils")
 
--- Function to set up the file explorer
-local function setup()
-
-    -- Stops executing if the packages aren't installed
-    if not utils.status_ok("mini.files") then return end
-
-    -- Set up mini files
-    require("mini.files").setup {
-
-        -- File explorer window options
-        windows = {
-
-            -- Turn on directory and file previewing
-            preview = true,
-
-            -- Set the width of the focused window
-            width_focus = 30,
-
-            -- Set the width of the non-focused window
-            -- width_nofocus = 75,
-
-            -- Set the width of the preview window
-            width_preview = 30
-        }
-    }
-
-end
-
 -- Returns the file explorer plugin for lazy.nvim
 return {
-    "echasnovski/mini.files",
-    version = false,
-    config = setup,
-    lazy = true,
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { open_for_directories = true },
     keys = {
-        { "<Leader>pw", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end, mode = "n", desc = "Opens the directory of the current file" },
-        { "<Leader>ec", function() require("mini.files").open("~/.config/nvim/lua/", true) end, mode = "n", desc = "Opens the Neovim config directory" }
-    }
+        {
+            "<Leader>pw",
+            function()
+                require("yazi").yazi()
+            end,
+            mode = "n",
+            desc = "Opens the directory of the current file",
+        },
+        {
+            "<Leader>ec",
+            function()
+                require("yazi").yazi(nil, "~/.config/nvim/lua/")
+            end,
+            mode = "n",
+            desc = "Opens the Neovim config directory",
+        },
+    },
 }
