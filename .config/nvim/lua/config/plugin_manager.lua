2af868d774495b95450836c70068eee534484e85
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-06-25 23:29:40 +0800

    Update the lazy.nvim bootstrap

--- .config/nvim/lua/config/plugin_manager.lua
+++ .config/nvim/lua/config/plugin_manager.lua
@@ -1,18 +1,18 @@
--- Plugin manager configuation
+-- Plugin manager configuration
 
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazy_path) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
-    lazypath,
+    lazy_path,
   })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazy_path)
 
 -- Plugin manager setup
 return require("lazy").setup("plugins")
