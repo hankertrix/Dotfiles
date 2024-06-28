61493516c1f0d2fb7b46135a50ab74663b3307b4
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-06-28 13:18:25 +0800

    Use tbl_extend to merge tables instead of unpack

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -62,29 +62,29 @@ local function setup()
         local opts = { buffer = bufnr, remap = false }
 
         -- LSP key bindings
-        vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, { unpack(opts), desc = descriptions["hover"] })
-        vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, { unpack(opts), desc = descriptions["definition"] })
-        vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, { unpack(opts), desc = descriptions["declaration"] })
-        vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, { unpack(opts), desc = descriptions["implementation"] })
-        vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, { unpack(opts), desc = descriptions["type_definition"] })
-        vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, { unpack(opts), desc = descriptions["references"] })
-        vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, { unpack(opts), desc = descriptions["signature_help"] })
-        vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, { unpack(opts), desc = descriptions["rename"] })
-        vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, { unpack(opts), desc = descriptions["format"] })
-        vim.keymap.set({ 'n', 'x' }, '<Leader>f', function() vim.lsp.buf.format({ async = true }) end, { unpack(opts), desc = descriptions["format"] })
-        vim.keymap.set('n', '<F4>', function() vim.lsp.buf.code_action() end, { unpack(opts), desc = descriptions["code_action"] })
+        vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, vim.tbl_extend("error", opts, { desc = descriptions["hover"] }))
+        vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, vim.tbl_extend("error", opts, { desc = descriptions["definition"] }))
+        vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, vim.tbl_extend("error", opts, { desc = descriptions["declaration"] }))
+        vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, vim.tbl_extend("error", opts, { desc = descriptions["implementation"] }))
+        vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, vim.tbl_extend("error", opts, { desc = descriptions["type_definition"] }))
+        vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, vim.tbl_extend("error", opts, { desc = descriptions["references"] }))
+        vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, vim.tbl_extend("error", opts, { desc = descriptions["signature_help"] }))
+        vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, vim.tbl_extend("error", opts, { desc = descriptions["rename"] }))
+        vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, vim.tbl_extend("error", opts, { desc = descriptions["format"] }))
+        vim.keymap.set({ 'n', 'x' }, '<Leader>f', function() vim.lsp.buf.format({ async = true }) end, vim.tbl_extend("error", opts, { desc = descriptions["format"] }))
+        vim.keymap.set('n', '<F4>', function() vim.lsp.buf.code_action() end, vim.tbl_extend("error", opts, { desc = descriptions["code_action"] }))
 
         -- If a range is selected and a range code action is available, use the range code action
         if vim.lsp.buf.range_code_action then
-            vim.keymap.set('x', '<F4>', function() vim.lsp.buf.range_code_action() end, { unpack(opts), desc = descriptions["code_action"] })
+            vim.keymap.set('x', '<F4>', function() vim.lsp.buf.range_code_action() end, vim.tbl_extend("error", opts, { desc = descriptions["code_action"] }))
         else
-            vim.keymap.set('x', '<F4>', function() vim.lsp.buf.code_action() end, { unpack(opts), desc = descriptions["code_action"] })
+            vim.keymap.set('x', '<F4>', function() vim.lsp.buf.code_action() end, vim.tbl_extend("error", opts, { desc = descriptions["code_action"] }))
         end
 
         -- Diagnostic key bindings
-        vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, { unpack(opts), desc = descriptions["diagnostic_window"] })
-        vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, { unpack(opts), desc = descriptions["diagnostic_prev"] })
-        vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, { unpack(opts), desc = descriptions["diagnostic_next"] })
+        vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, vim.tbl_extend("error", opts, { desc = descriptions["diagnostic_window"] }))
+        vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, vim.tbl_extend("error", opts, { desc = descriptions["diagnostic_prev"] }))
+        vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, vim.tbl_extend("error", opts, { desc = descriptions["diagnostic_next"] }))
     end)
 
     -- Gets the shared configs file
