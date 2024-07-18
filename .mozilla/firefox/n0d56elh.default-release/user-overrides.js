dc8d1d87b6f3072e3f63bff033a273997b8fa486
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-07-18 11:30:21 +0800

    Reformatted the user overrides file
    Also added the pref to disable privacy preserving ad measurement.

--- .mozilla/firefox/n0d56elh.default-release/user-overrides.js
+++ .mozilla/firefox/n0d56elh.default-release/user-overrides.js
@@ -1,40 +1,58 @@
 // My overrides for Arkenfox's user.js
 
+// Use the user js parrot pref to report if there's a syntax error
+// with the overrides section
+user_pref("_user.js.parrot", "ERROR: Overrides section syntax error");
+
 // Enable session restore
 user_pref("browser.startup.page", 3);
 
 // Get firefox to save new files automatically instead of asking
 user_pref("browser.download.always_ask_before_handling_new_types", false);
 
-// Enable autoscrolling
-// This is scrolling mode activated by the middle click button
+// Enable autoscrolling.
+// This is scrolling mode activated by the middle click button.
 user_pref("general.autoScroll", true);
 
-// Stop firefox from asking to save passwords
-// I have Bitwarden to do this
+// Stop firefox from asking to save passwords.
+// I have Bitwarden to do this.
 user_pref("signon.rememberSignons", false);
 
-// Disable capturing credentials in private browsing mode
-// I have Bitwarden to do this
+// Disable capturing credentials in private browsing mode.
+// I have Bitwarden to do this.
 user_pref("signon.privateBrowsingCapture.enabled", false);
 
-// Disable the credit card and address manager
-// I have Bitwarden to do this
+// Disable the credit card and address manager.
+// I have Bitwarden to do this.
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
 
-// Remove everything on the new tab page so that it looks clean
+// Remove everything on the new tab page so that it looks clean.
 // This actually doesn't matter since I use Vimium C's new tab adapter
-// Which already makes the page a blank page
-user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);              // Shortcuts
-user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);       // Sponsored shortcuts
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);    // Recommended by Pocket
-user_pref("browser.newtabpage.activity-stream.showSponsored", false);               // Sponsored Stories
+// which already makes the page a blank page.
 
-// Disable sending to pocket on the new tab page
-// Not that it really matters since the new tab page is already clean
-user_pref("browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled", false);
-user_pref("browser.newtabpage.activity-stream.discoverystream.saveToPocketCard.enabled", false);
+// Remove shortcuts
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
+// Remove sponsored shortcuts
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+
+// Remove recommended by Pocket
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+
+// Remove sponsored Stories
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+
+// Disable sending to pocket on the new tab page,
+// not that it really matters since the new tab page is already clean.
+user_pref(
+  "browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled",
+  false,
+);
+user_pref(
+  "browser.newtabpage.activity-stream.discoverystream.saveToPocketCard.enabled",
+  false,
+);
 
 // Disable pocket entirely
 user_pref("extensions.pocket.enabled", false);
@@ -72,24 +90,34 @@ user_pref("network.dns.skipTRR-when-parental-control-enabled", false);
 // Show the user suggestions page when there is a error in HTTPS-only mode
 user_pref("dom.security.https_only_mode_error_page_user_suggestions", true);
 
+// Allow the creation of custom search engines.
+// Secret pref.
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);
+
+// Disable privacy preserving ad measurement
+user_pref("dom.private-attribution.submission.enabled", false);
+
 // Don't clear history on shutdown
 user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);
-
-// Allow the creation of custom search engines
-// Secret pref
-user_pref("browser.urlbar.update2.engineAliasRefresh", true);
 
 // Resist fingerprinting
 user_pref("privacy.resistFingerprinting", true);
 
-// Enable letter boxing (the borders on the sides of the window to fake screen size)
+// Enable letter boxing
+// (the borders on the sides of the window to fake screen size)
 user_pref("privacy.resistFingerprinting.letterboxing", true);
 
-// Disable WebGL
-user_pref("webgl.disabled", true);
+// Exempt Telegram web from resist fingerprinting
+user_pref("privacy.resistFingerprinting.exemptedDomains", "web.telegram.org");
 
 // Spoof the language so that my Firefox locale doesn't match my OS locale
 user_pref("privacy.spoof_english", 2);
 
-// Exempt Telegram web from resist fingerprinting
-user_pref("privacy.resistFingerprinting.exemptedDomains", "web.telegram.org");
+// Disable WebGL
+user_pref("webgl.disabled", true);
+
+// Use the user js parrot pref to report if the overrides section is successful
+user_pref(
+  "_user.js.parrot",
+  "SUCCESS: No no he's not dead, he's, he's restin'!",
+);
