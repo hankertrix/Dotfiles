d8cea9e1d9a78bac6296300895c019782029231c
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-10-05 21:43:23 +0800

    Use Magit keybinds for Neogit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,17 +4,48 @@
 -- Gets the module with the utilities
 local utils = require("utils")
 
+-- The configuration for the git plugin
+local function config()
+    --
+
+    -- If the git plugin is not installed, then don't do anything
+    if not utils.status_ok("neogit") then return end
+
+    -- Setup the key binds to be the same as Magit
+    require("neogit").setup({
+        mappings = {
+            popup = {
+                ["F"] = "PullPopup",
+                ["p"] = "PushPopup",
+                ["P"] = "PushPopup",
+                ["_"] = "RevertPopup",
+                ["v"] = false,
+                ["O"] = "ResetPopup",
+                ["X"] = false,
+                ["z"] = "StashPopup",
+                ["Z"] = "WorktreePopup",
+                ["w"] = false,
+            },
+            status = {
+                ["X"] = "Untrack",
+                ["K"] = false,
+                ["<C-g>"] = "Close",
+            },
+        },
+    })
+end
+
 -- Returns the git plugin for lazy.nvim
 return {
     "NeogitOrg/neogit",
     dependencies = "nvim-lua/plenary.nvim",
     lazy = true,
     cond = utils.firenvim_not_active,
-    config = true,
+    config = config,
     cmd = "Neogit",
     keys = {
 
-        -- The keybind to open Neogit
+        -- The key bind to open Neogit
         {
             "<Leader>gs",
             function()
