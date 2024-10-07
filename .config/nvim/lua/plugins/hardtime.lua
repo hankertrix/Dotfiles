6370ae037ef422e60fb444f7590ab3c107e07da3
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-10-07 13:59:03 +0800

    Add hardtime.nvim

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,42 @@
+-- A module to make my life harder in Neovim
+-- but build better habits and make editing
+-- faster and more efficient.
+--
+-- Currently the plugin is hardtime.nvim
+
+-- Return the hardtime plugin for lazy.nvim
+return {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    cmd = { "Hardtime" },
+    config = function(_, opts)
+        --
+
+        -- Disable the showmode
+        -- so I can see the hint messages
+        vim.opt.showmode = false
+
+        -- Set up the plugin
+        require("hardtime").setup(opts)
+    end,
+    opts = {
+        hints = {
+
+            -- Hint to use b instead of ()
+            ["[dcyvV][ia][%(%)]"] = {
+                message = function(keys)
+                    return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
+                end,
+                length = 3,
+            },
+
+            -- Hint to use B instead of {}
+            ["[dcyvV][ia][%{%}]"] = {
+                message = function(keys)
+                    return "Use " .. keys:sub(1, 2) .. "B instead of " .. keys
+                end,
+                length = 3,
+            },
+        },
+    },
+}
