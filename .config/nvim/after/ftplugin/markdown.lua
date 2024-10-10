810bb01a18048dff8f1b559bf8fe977f62d80c1d
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-10-10 13:25:58 +0800

    Add back spellchecking
    But don't spell check markdown files, only txt files.

--- .config/nvim/after/ftplugin/markdown.lua
+++
@@ -1,4 +0,0 @@
--- Configuration for markdown files
-
--- Gets the spell checking configuration
-require("config.filetype.spellcheck_on")
---
+++ .config/nvim/lua/config/filetype/spellcheck_on.lua
@@ -0,0 +1,5 @@
+-- Spellchecking configuration
+
+-- Enables spell checking
+vim.opt.spell = true
+vim.opt.spelllang = { "en_gb" }
