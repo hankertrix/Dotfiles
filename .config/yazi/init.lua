2277a2d330dac2f2ac0d9f7db427e691fbe62b22
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-10-13 03:44:55 +0800

    Add the relative motion plugin

---
+++ .config/yazi/init.lua
@@ -0,0 +1,7 @@
+-- The init.lua file to configure the Yazi plugins I use
+
+-- Configure the relative motions plugin
+require("relative-motions"):setup({
+    show_numbers = "relative",
+    show_motion = true,
+})
