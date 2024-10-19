9825161a5a57d63f15a83c1ec3d9a5d918d0d7d6
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-10-19 14:05:30 +0800

    Refactor the theme.lua file

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -3,33 +3,24 @@
 -- Gets the module with the utilities
 local utils = require("utils")
 
--- Function to set up the themes
-local function setup()
-    --
-
-    -- The theme that I want to use
-    local theme = "bluloco"
-
-    -- Stops executing if the theme isn't installed
-    if not utils.status_ok(theme) then return end
-
-    -- If firenvim isn't active, sets the colour scheme to bluloco dark
-    if utils.firenvim_not_active() then
-        vim.cmd("colorscheme bluloco-dark")
-
-    -- Otherwise, sets the colour scheme to bluloco light
-    else
-        vim.cmd("colorscheme bluloco-light")
-    end
-end
-
 -- Returns the theme plugin for lazy.nvim
 return {
 
-    -- Bluloco theme
-    "uloco/bluloco.nvim",
-    config = setup,
-    lazy = false,
-    priority = 1000,
-    dependencies = { "rktjmp/lush.nvim" },
+    {
+        -- Bluloco theme
+        "uloco/bluloco.nvim",
+        lazy = false,
+        priority = 1000,
+        dependencies = { "rktjmp/lush.nvim" },
+        config = function()
+            --
+
+            -- Get the colour scheme I want to use
+            local colour_scheme = utils.firenvim_not_active() and "bluloco-dark"
+                or "bluloco-light"
+
+            -- Sets the theme
+            vim.cmd("colorscheme " .. colour_scheme)
+        end,
+    },
 }
