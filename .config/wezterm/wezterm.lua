df04a1affe84348fcb62045a5589370252b0afb3
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-11-21 15:37:49 +0800

    Reformatted the file

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,9 +6,7 @@ local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+if wezterm.config_builder then config = wezterm.config_builder() end
 
 -- This is where you actually apply your config choices
 
