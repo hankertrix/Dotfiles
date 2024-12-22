5da81694b5bb4a7329aa0885d6b89510912fd094
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-12-22 13:55:02 +0800

    Use bluloco colourscheme auto background detection to set the theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -16,8 +16,7 @@ return {
             --
 
             -- Get the colour scheme I want to use
-            local colour_scheme = utils.firenvim_not_active() and "bluloco-dark"
-                or "bluloco-light"
+            local colour_scheme = "bluloco"
 
             -- Sets the theme
             vim.cmd("colorscheme " .. colour_scheme)
