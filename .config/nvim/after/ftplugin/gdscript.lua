9d9fecb882f9b2413c2e52bed758c23d16902ee9
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-01-11 21:46:55 +0800

    Add the configuration for GDScript

---
+++ .config/nvim/after/ftplugin/gdscript.lua
@@ -0,0 +1,25 @@
+-- Configuration for gdscript files
+
+-- Get the port to connect to the Godot LSP
+local port = tonumber(os.getenv("GDScript_Port") or "6005")
+
+-- The command to connect to the Godot LSP
+local command = vim.lsp.rpc.connect("127.0.0.1", port)
+
+-- The pipe to communicate with the Godot LSP
+local pipe = "/tmp/godot.pipe"
+
+-- Start the lsp
+vim.lsp.start({
+    name = "Godot",
+    cmd = command,
+    root_dir = vim.fs.dirname(
+        unpack(vim.fs.find({ "project.godot", ".git" }, { upward = true }))
+    ),
+    on_attach = function(_, _)
+        local server_list = vim.fn.serverlist()
+        if not vim.tbl_contains(server_list, pipe) then
+            vim.api.nvim_command(string.format('echo serverstart("%s")', pipe))
+        end
+    end,
+})
