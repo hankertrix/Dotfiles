97c3cf7de6bd60f6f51aa72643c413627819b48b
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-01-14 01:06:19 +0800

    Fixed the titlecase function

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -9,7 +9,7 @@ function M.firenvim_not_active() return not vim.g.started_by_firenvim end
 -- Function to titlecase a string
 function M.titlecase(str)
     return str:gsub(
-        "(%l)(%w*)",
+        "(%a)(%w*)",
         function(first_character, rest_of_the_word)
             return string.upper(first_character) .. rest_of_the_word
         end
