47577c1551169da2be7b16ae55b5aac492fb4616
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-01-14 17:06:26 +0800

    Always use Firenvim on GitHub
    Also, set the file type to Markdown on GitHub.

--- .config/nvim/lua/plugins/firenvim.lua
+++ .config/nvim/lua/plugins/firenvim.lua
@@ -1,26 +1,45 @@
--- Configuration for firenvim
+-- Configuration for Firenvim
 
--- The settings for firenvim
+-- The settings for Firenvim
 local local_settings = {
     [".*"] = {
 
         -- Use Neovim's command line
         cmdline = "neovim",
 
-        -- Never automatically use firenvim
+        -- Never automatically use Firenvim
         takeover = "never",
+    },
+
+    -- GitHub settings
+    ["https?://github.com/"] = {
+
+        -- Always takeover
+        takeover = "always",
+
+        -- Make the configuration of a higher priority
+        priority = 1,
     },
 }
 
--- Set up the firenvim configuration
+-- Set up the Firenvim configuration
 vim.g.firenvim_config = {
     localSettings = local_settings,
 }
 
--- Returns the firenvim plugin for lazy.nvim
+-- Returns the Firenvim plugin for lazy.nvim
 return {
     "glacambre/firenvim",
     lazy = require("utils").firenvim_not_active(),
     module = false,
     build = ":call firenvim#install(0)",
+    config = function(_, _)
+        --
+
+        -- Set the file type to mark down on GitHub
+        vim.api.nvim_create_autocmd("BufEnter", {
+            pattern = "github.com_*.txt",
+            command = "set filetype=markdown",
+        })
+    end,
 }
