56a271abdeda3d15cac0b7037c6526b786195de7
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-01-25 15:00:52 +0800

    Disable the completion engine in the Typr buffer

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -35,6 +35,13 @@ return {
     -- Configuration options
     opts = {
 
+        -- Function to decide when to enable the completion engine
+        enabled = function()
+            return not vim.tbl_contains({ "typr" }, vim.bo.filetype)
+                and vim.bo.buftype ~= "prompt"
+                and vim.b.completion ~= false
+        end,
+
         -- Sources configuration
         sources = {
 
