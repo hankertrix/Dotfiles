f20d53ebbab3162777ced10a7c27d7f5b10357ad
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-02-07 19:24:45 +0800

    Edit the smart column configuration to use my own plugin

--- .config/nvim/lua/plugins/smart_column.lua
+++ .config/nvim/lua/plugins/smart_column.lua
@@ -1,22 +1,24 @@
 -- The configuration for the smart column plugin
--- Currently it's smartcolumn.nvim
+-- Currently it's nerd_column.nvim
 
 -- Gets the module with the shared configurations
 local shared_configs = require("shared_configs")
 
 -- Returns the smart column plugin for lazy.nvim
 return {
-	"m4xshen/smartcolumn.nvim",
+	"hankertrix/nerd_column.nvim",
+	dev = true,
+	dir = "~/projects/nerd_column.nvim",
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 	opts = {
 
-		-- Show a line on the right that represents the maximum column width
-		-- This makes it easier to keep to a character limit, usually 80
-		colorcolumn = tostring(shared_configs.max_line_length),
+		-- Show a line on the right that represents the maximum column width.
+		-- This makes it easier to keep to a character limit, usually 80.
+		colour_column = shared_configs.max_line_length,
 
 		-- Disable the plugins for the filetypes below
-		disabled_filetypes = vim.list_extend(
-			{ "text", "bigfile" },
+		disabled_file_types = vim.list_extend(
+			{ "text" },
 			shared_configs.disabled_file_types
 		),
 	},
