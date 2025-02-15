d33b9ecdb6c5e1d31ce9e429246e954fb00099ee
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-02-15 17:25:19 +0800

    Add configuration for the git plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -5,3 +5,30 @@ require("relative-motions"):setup({
 	show_numbers = "relative",
 	show_motion = true,
 })
+
+-- Configure the git plugin
+THEME.git = THEME.git or {
+
+	-- Colours
+	modified = ui.Style():fg("#0096DB"),
+	added = ui.Style():fg("#239549"),
+	untracked = ui.Style():fg("#B0B0B0"),
+	ignored = ui.Style():fg("#B0B0B0"),
+	deleted = ui.Style():fg("#D32752"),
+
+	-- Unmerged
+	updated = ui.Style():fg("#CD32C0"),
+
+	-- Icons
+	modified_sign = "",
+	added_sign = "",
+	untracked_sign = "󱋽",
+	ignored_sign = "",
+	deleted_sign = "",
+
+	-- Unmerged
+	updated_sign = "",
+}
+
+-- Set up the git plugin
+require("git"):setup()
