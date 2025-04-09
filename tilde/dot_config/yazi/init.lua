dfe269b3ac1f6a466a08089696d68d8064623cd4
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-04-09 08:18:04 +0800

    Reformatted the Yazi init.lua file

--- tilde/dot_config/yazi/init.lua
+++ tilde/dot_config/yazi/init.lua
@@ -7,28 +7,29 @@ require("relative-motions"):setup({
 })
 
 -- Configure the git plugin
-th.git = th.git or {
+th.git = th.git
+	or {
 
-	-- Colours
-	modified = ui.Style():fg("#0096DB"),
-	added = ui.Style():fg("#239549"),
-	untracked = ui.Style():fg("#B0B0B0"),
-	ignored = ui.Style():fg("#B0B0B0"),
-	deleted = ui.Style():fg("#D32752"),
+		-- Colours
+		modified = ui.Style():fg("#0096DB"),
+		added = ui.Style():fg("#239549"),
+		untracked = ui.Style():fg("#B0B0B0"),
+		ignored = ui.Style():fg("#B0B0B0"),
+		deleted = ui.Style():fg("#D32752"),
 
-	-- Unmerged
-	updated = ui.Style():fg("#CD32C0"),
+		-- Unmerged
+		updated = ui.Style():fg("#CD32C0"),
 
-	-- Icons
-	modified_sign = "",
-	added_sign = "",
-	untracked_sign = "󱋽",
-	ignored_sign = "",
-	deleted_sign = "",
+		-- Icons
+		modified_sign = "",
+		added_sign = "",
+		untracked_sign = "󱋽",
+		ignored_sign = "",
+		deleted_sign = "",
 
-	-- Unmerged
-	updated_sign = "",
-}
+		-- Unmerged
+		updated_sign = "",
+	}
 
 -- Set up the git plugin
 require("git"):setup()
