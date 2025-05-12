5f87cd04575a6ecc65dd3ef3c7c4099922f77d35
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-05-12 12:53:47 +0800

    Remove spell checking configuration

--- tilde/dot_config/nvim/after/ftplugin/text.lua
+++
@@ -1,4 +0,0 @@
--- Configuration for txt files
-
--- Gets the spell checking configuration
-require("config.filetype.spellcheck_on")
--- tilde/dot_config/nvim/lua/config/filetype/spellcheck_on.lua
+++
@@ -1,5 +0,0 @@
--- Spellchecking configuration
-
--- Enables spell checking
-vim.opt.spell = true
-vim.opt.spelllang = { "en_gb" }
