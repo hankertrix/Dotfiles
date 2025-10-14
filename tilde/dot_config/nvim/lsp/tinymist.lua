1daca6ecaf0b4e7a8dc38960aa9dd76ee282e478
Hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-10-14 15:18:37 +0800

    Only export PDF, lint and refresh the preview on save for Typst

--- tilde/dot_config/nvim/lsp/tinymist.lua
+++ tilde/dot_config/nvim/lsp/tinymist.lua
@@ -2,12 +2,16 @@
 return {
 	settings = {
 		formatterMode = "typstyle",
-		exportPdf = "onType",
+		exportPdf = "onSave",
 		formatterIndentSize = 4,
 		formatterPrintWidth = require("shared_configs").max_line_length,
 		formatterProseWrap = true,
 		lint = {
 			enabled = true,
+			when = "onSave",
+		},
+		preview = {
+			refresh = "onSave",
 		},
 	},
 }
