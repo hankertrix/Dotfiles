cdf656079e74388f5a995ad62b343a5467694c2d
Hankertrix <91734413+hankertrix@users.noreply.github.com>
2026-05-11 23:28:06 +0800

    Update to use plugin-specific styles

--- tilde/dot_config/yazi/init.lua
+++ tilde/dot_config/yazi/init.lua
@@ -6,31 +6,5 @@ require("relative-motions"):setup({
 	show_motion = true,
 })
 
--- Configure the git plugin
----@diagnostic disable-next-line: inject-field
-th.git = th.git
-	or {
-
-		-- Colours
-		modified = ui.Style():fg("#0096DB"),
-		added = ui.Style():fg("#239549"),
-		untracked = ui.Style():fg("#B0B0B0"),
-		ignored = ui.Style():fg("#B0B0B0"),
-		deleted = ui.Style():fg("#D32752"),
-
-		-- Unmerged
-		updated = ui.Style():fg("#CD32C0"),
-
-		-- Icons
-		modified_sign = "",
-		added_sign = "",
-		untracked_sign = "󱋽",
-		ignored_sign = "",
-		deleted_sign = "",
-
-		-- Unmerged
-		updated_sign = "",
-	}
-
 -- Set up the git plugin
 require("git"):setup()
